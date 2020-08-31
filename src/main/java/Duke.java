705f62f07da15189d7e758ca52866fb05c1a15a0
xX-Conan-Xx <61041807+xX-Conan-Xx@users.noreply.github.com>
2020-08-31 18:01:40 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
         String line;
+        ArrayList<String> requests = new ArrayList<String>();
         Scanner in = new Scanner(System.in);
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -17,9 +19,20 @@ public class Duke {
         System.out.println(SPLIT_LINE);
         line = in.nextLine();
         while (!line.equals("bye")){
-            System.out.println(SPLIT_LINE);
-            System.out.println(line);
-            System.out.println(SPLIT_LINE);
+            if(!line.equals("list")){
+                System.out.println(SPLIT_LINE);
+                System.out.println("added: "+line);
+                requests.add(line);
+                System.out.println(SPLIT_LINE);
+            }
+            else{
+                System.out.println(SPLIT_LINE);
+                for (int i = 0; i < requests.size(); i++) {
+
+                    System.out.println((i+1)+":"+requests.get(i));
+                }
+                System.out.println(SPLIT_LINE);
+            }
             line = in.nextLine();
         }
         System.out.println(" Bye. Hope to see you again soon!");
---
+++ src/main/java/Request.java
@@ -0,0 +1,10 @@
+public class Request {
+    private String requestContent;
+
+    Request (String requestContent){
+        requestContent = requestContent;
+    }
+    public String getContent(){
+        return requestContent;
+    }
+}
