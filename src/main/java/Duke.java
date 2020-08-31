58d298a2978f8d3b79932a149754497c7853a724
xX-Conan-Xx <61041807+xX-Conan-Xx@users.noreply.github.com>
2020-08-31 17:06:53 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line;
+        Scanner in = new Scanner(System.in);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        final String SPLIT_LINE = "____________________________________________________________";
+        System.out.println(SPLIT_LINE);
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        System.out.println(SPLIT_LINE);
+        line = in.nextLine();
+        while (!line.equals("bye")){
+            System.out.println(SPLIT_LINE);
+            System.out.println(line);
+            System.out.println(SPLIT_LINE);
+            line = in.nextLine();
+        }
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(SPLIT_LINE);
     }
 }
