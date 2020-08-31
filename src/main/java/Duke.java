6f5f8bef731e13aad890603f1a3feeceff57b76d
xX-Conan-Xx <61041807+xX-Conan-Xx@users.noreply.github.com>
2020-08-31 18:54:52 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String line;
-        ArrayList<String> requests = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner in = new Scanner(System.in);
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -19,18 +19,24 @@ public class Duke {
         System.out.println(SPLIT_LINE);
         line = in.nextLine();
         while (!line.equals("bye")){
-            if(!line.equals("list")){
+            if(line.equals("list")){
+                System.out.println(SPLIT_LINE);
+                for (int i = 0; i < tasks.size(); i++) {
+
+                    System.out.println((i+1)+":"+"["+tasks.get(i).getStatusIcon()+"] "+tasks.get(i).description);
+                }
+                System.out.println(SPLIT_LINE);
+            }else if(line.startsWith("done")){
+                System.out.println(SPLIT_LINE);
+                int taskNumberCompleted = Integer.parseInt(line.replaceAll("\\D+",""))-1;
+                tasks.set(taskNumberCompleted,tasks.get(taskNumberCompleted).completeTask());
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println(" "+":"+"["+tasks.get(taskNumberCompleted).getStatusIcon()+"] "+tasks.get(taskNumberCompleted).description);
+                System.out.println(SPLIT_LINE);
+            } else{
                 System.out.println(SPLIT_LINE);
                 System.out.println("added: "+line);
-                requests.add(line);
-                System.out.println(SPLIT_LINE);
-            }
-            else{
-                System.out.println(SPLIT_LINE);
-                for (int i = 0; i < requests.size(); i++) {
-
-                    System.out.println((i+1)+":"+requests.get(i));
-                }
+                tasks.add(new Task(line));
                 System.out.println(SPLIT_LINE);
             }
             line = in.nextLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public Task(String description,boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public Task completeTask(){
+        return new Task(this.description,true);
+    }
+}
