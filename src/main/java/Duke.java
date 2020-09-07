1559d1d1eef60b703575b490cc720ef695c7f01c
xX-Conan-Xx <61041807+xX-Conan-Xx@users.noreply.github.com>
2020-09-07 23:22:40 +0800

    Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,51 +13,71 @@ public class Duke {
         System.out.println(SPLIT_LINE);
         line = in.nextLine();
         while (!line.equals("bye")){
-            if(line.equals("list")){
-                System.out.println(SPLIT_LINE);
-                for (int i = 0; i < tasks.size(); i++) {
+            try{
+                if(line.equals("list")){
+                    System.out.println(SPLIT_LINE);
+                    for (int i = 0; i < tasks.size(); i++) {
 
-                    System.out.println((i+1)+":"+tasks.get(i));
-                }
-                System.out.println(SPLIT_LINE);
-            }else if(line.startsWith("done")){
-                System.out.println(SPLIT_LINE);
-                int taskNumberCompleted = Integer.parseInt(line.replaceAll("\\D+",""))-1;
-                tasks.set(taskNumberCompleted,tasks.get(taskNumberCompleted).completeTask());
-                System.out.println(" Nice! I've marked this task as done:");
-                System.out.println(" "+":"+tasks.get(taskNumberCompleted));
-                System.out.println(SPLIT_LINE);
-            } else if(line.startsWith("todo")){
-                System.out.println(SPLIT_LINE);
-                System.out.println(" Got it. I've added this task:");
-                tasks.add(new Todo(line));
-                System.out.println("  "+new Todo(line));
-                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                System.out.println(SPLIT_LINE);
+                        System.out.println((i+1)+":"+tasks.get(i).toString());
+                    }
+                    System.out.println(SPLIT_LINE);
+                }else if(line.startsWith("done")){
+                    System.out.println(SPLIT_LINE);
+                    int taskNumberCompleted = Integer.parseInt(line.replaceAll("\\D+",""))-1;
+                    tasks.set(taskNumberCompleted,tasks.get(taskNumberCompleted).completeTask());
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println(" "+":"+tasks.get(taskNumberCompleted));
+                    System.out.println(SPLIT_LINE);
+                } else if(line.startsWith("todo")){
+                    System.out.println(SPLIT_LINE);
+                    System.out.println(" Got it. I've added this task:");
+                    tasks.add(new Todo(line));
+                    System.out.println("  "+new Todo(line));
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(SPLIT_LINE);
 //                System.out.println(SPLIT_LINE);
 //                System.out.println("added: "+line);
 //                tasks.add(new Task(line));
 //                System.out.println(SPLIT_LINE);
-            } else if(line.startsWith("deadline")){
-                System.out.println(SPLIT_LINE);
-                System.out.println(" Got it. I've added this task:");
-                tasks.add(new Deadline(line));
-                System.out.println("  "+new Deadline(line));
-                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                System.out.println(SPLIT_LINE);
-            }
-            else if(line.startsWith("event")){
-                System.out.println(SPLIT_LINE);
-                System.out.println(" Got it. I've added this task:");
-                tasks.add(new Event(line));
-                System.out.println("  "+new Event(line));
-                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                System.out.println(SPLIT_LINE);
-            }
-            else {
-                System.out.println(SPLIT_LINE);
-                System.out.println(" Cannot understand your words." + System.lineSeparator() + " Type bye to exit.");
-                System.out.println(SPLIT_LINE);
+                } else if(line.startsWith("deadline")){
+                    System.out.println(SPLIT_LINE);
+                    System.out.println(" Got it. I've added this task:");
+                    tasks.add(new Deadline(line));
+                    System.out.println("  "+new Deadline(line));
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(SPLIT_LINE);
+                }
+                else if(line.startsWith("event")){
+                    System.out.println(SPLIT_LINE);
+                    System.out.println(" Got it. I've added this task:");
+                    tasks.add(new Event(line));
+                    System.out.println("  "+new Event(line));
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(SPLIT_LINE);
+                }
+                else {
+                    System.out.println(SPLIT_LINE);
+                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                    System.out.println(SPLIT_LINE);
+                }
+            }catch (Exception e){
+                if(in.equals("todo"))
+                {   System.out.println(SPLIT_LINE);
+                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                    System.out.println(SPLIT_LINE);
+                }
+                if(in.equals("deadline"))
+                {   System.out.println(SPLIT_LINE);
+                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                    System.out.println(SPLIT_LINE);
+                }
+                if(in.equals("event"))
+                {   System.out.println(SPLIT_LINE);
+                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                    System.out.println(SPLIT_LINE);
+                }
+
+
             }
             line = in.nextLine();
         }
